b5874d7ea3fc1169c021ed87c7963ce7daf22f94
Raghav Seth <sethraghav12345@gmail.com>
2025-10-20 14:43:30 +0530

    trpc

---
+++ src/app/Client.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { trpc } from "@/trpc/server";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { use } from "react";
+
+export const Client = () => {
+
+    const trpc = useTRPC();
+    const {data: users} = useSuspenseQuery(trpc.getUsers.queryOptions());
+
+
+    return (
+        <div>
+            {JSON.stringify(users)}
+        </div>
+    );
+};
