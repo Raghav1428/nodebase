1ea184bf8147f870ca78f18eda48df509025c25c
Raghav Seth <sethraghav12345@gmail.com>
2025-11-22 04:41:44 +0530

    feat: Implement Inngest-powered workflow execution with node-based processing and topological sorting.

--- src/features/editor/components/execute-workflow-button.tsx
+++ src/features/editor/components/execute-workflow-button.tsx
@@ -1,15 +1,24 @@
 import { Button } from "@/components/ui/button";
-import { FlaskConicalIcon } from "lucide-react";
+import { useExecuteWorkflow } from "@/features/workflows/hooks/use-workflows";
+import { FlaskConicalIcon, Loader2Icon } from "lucide-react";
 
 export const ExecuteWorkflowButton = ({
     workflowId,
 }: {
     workflowId: string;
 }) => {
+
+    const executeWorkflow = useExecuteWorkflow();
+
+    const handleExecute = () => {
+        executeWorkflow.mutate({ id: workflowId });
+    };
+
     return (
-        <Button size="lg" onClick={() => {}} disabled={false}>
+        <Button size="lg" onClick={handleExecute} disabled={executeWorkflow.isPending}>
             <FlaskConicalIcon className="size-4" />
             Execute Workflow
+            {executeWorkflow.isPending && <Loader2Icon className="size-4 animate-spin" />}
         </Button>
     )
 };
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,58 +1,49 @@
-import prisma from "@/lib/db";
+import { NonRetriableError } from "inngest";
 import { inngest } from "./client";
-import { createGoogleGenerativeAI } from '@ai-sdk/google';
-import { generateText } from 'ai';
-import { createAnthropic } from '@ai-sdk/anthropic';
-import { createOpenAI } from '@ai-sdk/openai';
+import prisma from "@/lib/db";
+import { topologicalSort } from "./utils";
+import { NodeType } from "@/generated/prisma";
+import { getExecutor } from "@/features/executions/lib/executor-registry";
 
-const google = createGoogleGenerativeAI();
-const openai = createOpenAI();
-const anthropic = createAnthropic();
-
-export const execute = inngest.createFunction(
-  { id: "execute-ai" },
-  { event: "execute/ai" },
+export const executeWorkflow = inngest.createFunction(
+  { id: "execute-workflow" },
+  { event: "workflows/execute.workflow" },
   async ({ event, step }) => {
 
-    await step.sleep("pretend", "5s")
+    const workflowId  = event.data.workflowId;
 
-    const { steps: geminiSteps } = await step.ai.wrap("gemini-generate-text", generateText, {
-        model: google("gemini-2.5-flash"),
-        system: "You are a helpful assistant.",
-        prompt: "What is 2+2 ?",
-        experimental_telemetry: {
-          isEnabled: true,
-          recordInputs: true,
-          recordOutputs: true,
+    if(!workflowId) throw new NonRetriableError("Workflow ID is missing");
+
+    const sortedNodes = await step.run("prepare-workflow", async () => {
+      const workflow = await prisma.workflow.findUniqueOrThrow({
+        where: {
+          id: workflowId,
         },
+        include: {
+          nodes: true,
+          connections: true,
+        }
+      });
+
+      return topologicalSort(workflow.nodes, workflow.connections);
     });
 
-    const { steps: opeanaiSteps } = await step.ai.wrap("openai-generate-text", generateText, {
-        model: openai("gpt-3.5-turbo"),
-        system: "You are a helpful assistant.",
-        prompt: "What is 2+2 ?",
-        experimental_telemetry: {
-          isEnabled: true,
-          recordInputs: true,
-          recordOutputs: true,
-        },
-    });
+    // initialize context with any initial data from the trigger
+    let context = event.data.initialData || {};
 
-    const { steps: anthropoicSteps } = await step.ai.wrap("anthropic-generate-text", generateText, {
-        model: anthropic("claude-3-5-haiku-20241022"),
-        system: "You are a helpful assistant.",
-        prompt: "What is 2+2 ?",
-        experimental_telemetry: {
-          isEnabled: true,
-          recordInputs: true,
-          recordOutputs: true,
-        },
-    });
+    for(const node of sortedNodes) {
+      const executor = getExecutor(node.type as NodeType);
+      context = await executor({
+        data: node.data as Record<string, unknown>,
+        nodeId: node.id,
+        context,
+        step
+      })
+    }
 
-    return {
-        geminiSteps,
-        opeanaiSteps,
-        anthropoicSteps
-    };
+    return { 
+      workflowId,
+      result: context,
+     };
   },
 );
