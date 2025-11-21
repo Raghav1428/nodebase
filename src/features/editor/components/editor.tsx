76938cd49c0ab9a3fd263498651e2649f40a3fd6
Raghav Seth <sethraghav12345@gmail.com>
2025-11-22 02:49:31 +0530

    feat: Implement core workflow editor with HTTP request node configuration and workflow execution button.

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useMemo } from 'react';
 import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge, type Node, type Edge, type NodeChange, type EdgeChange, type Connection, Background, Controls, MiniMap, useReactFlow, Panel} from '@xyflow/react';
 import { ErrorView, LoadingView } from "@/components/entity-components";
 import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows";
@@ -12,6 +12,8 @@ import { nodeComponents } from '@/config/node-components';
 import { AddNodeButton } from './add-node-button';
 import { useSetAtom } from 'jotai';
 import { editorAtom } from '../store/atoms';
+import { NodeType } from '@/generated/prisma';
+import { ExecuteWorkflowButton } from './execute-workflow-button';
 
 export const EditorLoading = () => {
     return <LoadingView message="Loading editor..." />
@@ -42,6 +44,10 @@ export const Editor = ({ workflowId }: { workflowId: string }) => {
         [],
     );
 
+    const hasManualTrigger = useMemo(() =>{
+        return nodes.some(node => node.type === NodeType.MANUAL_TRIGGER);
+    }, [nodes]);
+
     return (
         <div className='size-full'>
             <ReactFlow
@@ -65,6 +71,11 @@ export const Editor = ({ workflowId }: { workflowId: string }) => {
                 <Panel position="top-right">
                     <AddNodeButton />
                 </Panel>
+                {hasManualTrigger && (
+                    <Panel position="bottom-center">
+                        <ExecuteWorkflowButton workflowId={workflowId} />
+                    </Panel>
+                )}
             </ReactFlow>
         </div>
     );
