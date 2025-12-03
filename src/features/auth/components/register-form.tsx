18e8c0a9c296606fa7201aa87a466cdd26fa35b8
Raghav Seth <sethraghav12345@gmail.com>
2025-12-04 03:44:52 +0530

    feat: Implement user authentication with oauth

--- src/features/auth/components/register-form.tsx
+++ src/features/auth/components/register-form.tsx
@@ -36,6 +36,32 @@ export function RegisterForm() {
         },
     });
 
+    const signInGithub = async () => {
+    const data = await authClient.signIn.social({
+        provider: "github"
+    }, {
+        onSuccess: () => {
+            router.push("/");
+        },
+        onError: (error) => {
+            toast.error(error.error.message || "Something went wrong");
+        },
+    });
+    };
+
+    const signInGoogle = async () => {
+    const data = await authClient.signIn.social({
+        provider: "google"
+    }, {
+        onSuccess: () => {
+            router.push("/");
+        },
+        onError: (error) => {
+            toast.error(error.error.message || "Something went wrong");
+        },
+    });
+    };
+
     const onSubmit = async (values: RegisterFormValues) => {
         await authClient.signUp.email(
             {
@@ -74,6 +100,7 @@ export function RegisterForm() {
                             <div className="grid gap-6">
                                 <div className="flex flex-col gap-4">
                                     <Button 
+                                        onClick={signInGithub}
                                         variant="outline"
                                         className="w-fulll"
                                         type="button"
@@ -82,6 +109,7 @@ export function RegisterForm() {
                                         Continue with GitHub
                                     </Button>
                                     <Button 
+                                        onClick={signInGoogle}
                                         variant="outline"
                                         className="w-fulll"
                                         type="button"
